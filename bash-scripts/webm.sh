06e5eb5e724f0ffd15e1a70522a0c13789ec5e26
serii <seriiburduja@gmail.com>
2024-02-06 09:47:04 +0200

    feat: webm.sh

--- bash-scripts/webm.sh
+++ bash-scripts/webm.sh
@@ -13,7 +13,13 @@
 #   ffmpeg -i "$NAME" "${NAME%.*}.mp3"
 # done
 
-for FILE in *.webm; do
+# for FILE in *.webm; do
+#     echo -e "Processing video '\e[32m$FILE\e[0m'";
+#     ffmpeg -i "${FILE}" -vn -ab 128k -ar 44100 -y "${FILE%.webm}.mp3";
+# done;
+
+for FILE in *.wav; do
     echo -e "Processing video '\e[32m$FILE\e[0m'";
+    # ffmpeg -i "${FILE}" "${FILE%.webm}.mp3"; 
     ffmpeg -i "${FILE}" -vn -ab 128k -ar 44100 -y "${FILE%.webm}.mp3";
 done;
