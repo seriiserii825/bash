1ba9de66489a8047bd4218a7c6dde324d4826043
serii <seriiburduja@gmail.com>
2024-02-15 16:37:58 +0200

    feat: git.sh chrome extension

--- bash-scripts/git.sh
+++ bash-scripts/git.sh
@@ -4,7 +4,7 @@ args=("$@")
 
 date_now=$(date +"%Y-%m-%d %H:%M:%S")
 function gitUpdateAll(){
-  dirs=(~/Documents/Knowledge-base_ ~/xubuntu ~/.password-store ~/.config/nvim ~/i3wm-home ~/i3wm-office ~/Documents/bash ~/Documents/python-info ~/Documents/python-scripts)
+  dirs=(~/Documents/Knowledge-base_ ~/xubuntu ~/.password-store ~/.config/nvim ~/i3wm-home ~/i3wm-office ~/Documents/bash ~/Documents/python-info ~/Documents/python-scripts ~/Documents/chrome-extenstions/tabs-copy)
   for dir in "${dirs[@]}"; do
     echo "=========================================="
     echo "${tblue}Update for $dir${treset}"
