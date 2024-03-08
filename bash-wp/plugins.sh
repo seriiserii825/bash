c1449614cb13ebf9780bf696bcae098764ed931e
serii <seriiburduja@gmail.com>
2024-03-08 18:01:13 +0200

    feat: finished with plugins

--- bash-wp/plugins.sh
+++ bash-wp/plugins.sh
@@ -1,4 +1,4 @@
-#! /bin/bash  
+#! /bin/bash 
 source /home/serii/Documents/bash/bash-scripts/bash-libs/printArray.sh
 source /home/serii/Documents/bash/bash-scripts/bash-libs/multipleSelect.sh
 # check if don't exists file front-page.php
@@ -12,39 +12,38 @@ currrent_path=$(pwd)
 plugin_dir="$(dirname "$(dirname "$currrent_path")")/plugins/*"
 
 local_plugins=( 
-  "advanced-custom-fields-pro-6_0_6.zip",
-  "all-in-one-wp-migration-7-79.zip",
-  "seo-by-rank-math.zip",
-  "wpglobus.zip",
-  "wpglobus-plus.zip",
-  "advanced-bulk-edit-v1.3.zip",
-  "all-in-one-wp-migration-7-79.zip"
+  "advanced-custom-fields-pro-6_0_6"
+  "all-in-one-wp-migration-7-79"
+  "seo-by-rank-math"
+  "wpglobus"
+  "wpglobus-plus"
+  "advanced-bulk-edit-v1.3"
 )
 
 server_plugins=(
-  "classic-editor",
-  "tinymce-advanced",
-  "stops-core-theme-and-plugin-updates",
-  "safe-svg",
-  "contact-form-7",
-  "contact-form-7-honeypot",
-  "wp-mail-smtp",
-  "cookie-notice",
-  "wps-hide-login",
-  "seo-by-rank-math",
-  "wp-pagenavi",
-  "error-log-monitor",
-  "query-monitor",
-  "post-duplicator",
-  "woocommerce",
-  "easy-woocommerce-auto-sku-generator",
-  "wc-fields-factory",
-  "webp-express",
-  "3d-flipbook-dflip-lite",
-  "flow-flow-social-streams",
-  "add-to-any",
-  "woo-ajax-mini-cart",
-  "wp-smushit",
+  "classic-editor"
+  "tinymce-advanced"
+  "stops-core-theme-and-plugin-updates"
+  "safe-svg"
+  "contact-form-7"
+  "contact-form-7-honeypot"
+  "wp-mail-smtp"
+  "cookie-notice"
+  "wps-hide-login"
+  "seo-by-rank-math"
+  "wp-pagenavi"
+  "error-log-monitor"
+  "query-monitor"
+  "post-duplicator"
+  "woocommerce"
+  "easy-woocommerce-auto-sku-generator"
+  "wc-fields-factory"
+  "webp-express"
+  "3d-flipbook-dflip-lite"
+  "flow-flow-social-streams"
+  "add-to-any"
+  "woo-ajax-mini-cart"
+  "wp-smushit"
 )
 
 function getInstalledPlugins(){
@@ -54,16 +53,17 @@ function getInstalledPlugins(){
 
 
 function installPlugins(){
-  local local_plugins=$1
-  local server_plugins=$2
-  for plugin in "${local_plugins[@]}"
-  do
-    wp plugin install ~/Documents/plugins-wp/$plugin --activate
-  done
-
-  for plugin in "${server_plugins[@]}"
-  do
-    wp plugin install $plugin --activate
+  local all_plugins=("${local_plugins[@]}" "${server_plugins[@]}")
+  local installed_plugins=($(getInstalledPlugins))
+  local plugins_to_install=($(echo ${all_plugins[@]} ${installed_plugins[@]} | tr ' ' '\n' | sort | uniq -u))
+  selected_plugins=($(multipleSelect "${plugins_to_install[@]}"))
+  for plugin in "${selected_plugins[@]}"
+  do 
+    if [[ " ${local_plugins[@]} " =~ " ${plugin} " ]]; then
+      wp plugin install ~/Documents/plugins-wp/$plugin.zip --activate
+    else
+      wp plugin install $plugin --activate
+    fi
   done
 }
 
@@ -77,11 +77,11 @@ function uninstallPlugins(){
   done
 }
 
-select action in "List" "Instal" "Uninstall" "Exit"
+select action in "List" "Install" "Uninstall" "Exit"
 do
   case $action in
     "List" ) wp plugin list ;;
-    "Install" ) installPlugins "${local_plugins[*]}" "${server_plugins[*]}" ;;
+    "Install" ) installPlugins ;;
     "Uninstall") uninstallPlugins;;
     "Exit" ) exit;;
   esac
