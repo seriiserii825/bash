c2b7cc3bbc49fa871fa959a38a74724563e4f7e9
serii <seriiburduja@gmail.com>
2024-03-20 17:37:00 +0200

    feat: t.sh works

--- bash-scripts/t.sh
+++ bash-scripts/t.sh
@@ -13,15 +13,15 @@ read -p "Enter current language(en, it, ru, ro, de)" current_lang
 
 while ./clipnotify;
 do
-  SelectedText="$(xsel)"
-  CopiedText="$(xsel -b)"
+  # SelectedText="$(xsel)"
+  # CopiedText="$(xsel -b)"
   # echo "SelectedText: $SelectedText"
-  echo "CopiedText: $CopiedText"
-  sleep 1
+  # echo "CopiedText: $CopiedText"
+  sleep 2
   clipboard=$(xclip -o -selection clipboard)
+  echo "clipboard: $clipboard"
   echo "current_lang: $current_lang"
-  trans -b  ":$current_lang" "$clipboard"  | tr -d '\n' | xsel -b -i 
-  sleep 1
-  clipboard=$(xclip -o -selection clipboard)
-  notify-send "$(echo -e "$clipboard")"
+  trans -b  ":$current_lang" "$clipboard"  | tr -d '\n' | xsel -b -i && 
+  clipboard=$(xclip -o -selection clipboard)  &&
+  notify-send "$(echo -e "$clipboard")" 
 done
