4fccee1578c1ca1c0adc4ebce2dd952139ae6d32
serii <seriiburduja@gmail.com>
2024-05-22 16:32:39 +0300

    feat: replace in files with grep

--- bash-scripts/replace_in_files.sh
+++ bash-scripts/replace_in_files.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 read -p "Enter a word: " word
-read -p "Enter file_type: " file_type
-
-find -name "*.$file_type" -exec sed -i "s/$word/REPLACED/g" {} \;
+grep --color=always -Rn . -e $word
+read -p "Enter new word: " new_word
+read -p "Enter file_type, by comma: " file_type
+file_types=()
+IFS=',' read -r -a file_types <<< "$file_type"
+for file_type in "${file_types[@]}"
+do
+    echo "file_type: $file_type"
+    # find . -name "*.$file_type"  -exec sed -i "s/$word/$new_word/g" {} \;
+    find . -type d \( -name node_modules -o -name .git \) -prune -o -name "*.$file_type" -exec sed -i "s/$word/$new_word/g" {} \;
+done
