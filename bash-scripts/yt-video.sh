6a3652651d870e55a3a271467aabeeeb0fcb2e9b
serii <seriiburduja@gmail.com>
2024-06-05 17:39:14 +0300

    feat: yt-video

---
+++ bash-scripts/yt-video.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+youtube_url=$(xclip -o -selection clipboard)
+
+# Check if the URL is valid
+if [[ $youtube_url == *youtube* ]]; then
+  yt-dlp -o '%(title)s.%(ext)s' --format "bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 $youtube_url
+else
+  echo "${tmagenta}Error: Invalid YouTube URL.${treset}"
+fi
