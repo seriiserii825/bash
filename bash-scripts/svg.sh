fafff122d182d0d59648e3830730bf55638d74b9
serii <seriiburduja@gmail.com>
2024-06-07 09:01:25 +0300

    feat: svg sh

--- bash-scripts/svg.sh
+++ bash-scripts/svg.sh
@@ -3,8 +3,12 @@
 file_path=$( fzf )
 read -p "Enter width: " width
 file=$( basename "$file_path" .svg )
-echo "file: $file"
+new_file=$( echo "$file"-"$width".svg )
 
 rsvg-convert -a -w "$width" -f svg "$file".svg -o "$file"-"$width".svg
+svgo "$file"-"$width".svg
 
-echo "your file is ready: $file-$width.svg"
+cat "$file"-"$width".svg | xclip -selection clipboard
+echo "${tgreen}your file is ready: $file-$width.svg${treset}"
+bat "$file-$width.svg"
+echo "${tblue}Copied to clipboard!${treset}"
