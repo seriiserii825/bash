bc078a174ebe63272a8fc49e63d0d2fccb977fbb
serii <seriiburduja@gmail.com>
2024-06-21 12:35:19 +0300

    upd: optimize update

--- bash-scripts/optimize-mp4.sh
+++ bash-scripts/optimize-mp4.sh
@@ -3,23 +3,54 @@
 # choose video mp4 file with fzf
 video_url=$(find . -type f -name "*.mp4" | fzf)
 
-# if video don't end with .mp4
-if [[ $video_url != *.mp4 ]]; then
-  echo "${tmagenta}Please select a video file${treset}"
-fi
+function hasVideoAudio(){
+  ffprobe -i $video_url -show_streams 2>&1 | grep 'Stream #0:1'
+  if [ $? -eq 0 ]; then
+    echo "${tmagenta}Video has audio${treset}"
+  else
+    echo "${tmagenta}Video has no audio${treset}"
+  fi
+}
 
+function optimizeVideo(){
+  read -p "Enter quality by default is 24 (0-51): " quality
 
-read -p "Enter quality by default is 24 (0-51): " quality
-
-if [ -z "$quality" ]; then
+  if [ -z "$quality" ]; then
     quality=24
-fi
+  fi
 
-output_file=$(echo $video_url | sed "s/.mp4/_$quality.mp4/g")
 
-if [ -f $output_file ]; then
+  output_file=$(echo $video_url | sed "s/.mp4/_$quality.mp4/g")
+
+  if [ -f $output_file ]; then
     echo "${tmagenta}File already exists${treset}"
     read -p "Enter new file name: " output_file
-fi
+  fi
 
-ffmpeg -i $video_url -vcodec libx265 -crf $quality $output_file
+
+  read -p "Remove video audio? (y/n): " remove_audio
+
+  if [ "$remove_audio" == "y" ]; then
+    output_file=$(echo $video_url | sed "s/.mp4/_$quality-no-audio.mp4/g")
+    ffmpeg -i $video_url -vcodec libx265 -crf $quality -an $output_file
+  else
+    ffmpeg -i $video_url -vcodec libx265 -crf $quality $output_file
+  fi
+}
+
+select action in "hasAudio" "Optimize video" "Cancel"; do
+  case $action in
+    "hasAudio")
+      hasVideoAudio
+      ;;
+    "Optimize video")
+      optimizeVideo
+      ;;
+    "Cancel")
+      exit
+      ;;
+  esac
+done
+
+
+
