f1f1efee1580eb22759a91fdb293efac039e1ff0
serii <seriiburduja@gmail.com>
2024-06-24 13:33:18 +0300

    feat: mg rotate

--- bash-scripts/mg.sh
+++ bash-scripts/mg.sh
@@ -1,42 +1,50 @@
 #! /bin/bash
 
 function changeImage(){
-COLUMNS=1
-select size_direction in INFO OPTIMIZE WIDTH HEIGHT FLOP EXIT
-do
+  COLUMNS=1
+  select size_direction in INFO OPTIMIZE WIDTH HEIGHT FLOP ROTATE EXIT
+  do
     case $size_direction in
-        INFO)
-          echo -e "${tblue}Resolution of the image${treset}"
-          identify -format "%wx%h\n" $*
+      INFO)
+        echo -e "${tblue}Resolution of the image${treset}"
+        identify -format "%wx%h\n" $*
 
-          echo -e "${tgreen}Size of the image${treset}"
-          du -sh $*
-            ;;
-        OPTIMIZE)
-          jpegoptim --strip-all --all-progressive -ptm 80 $*
-            ;;
-        WIDTH)
-          echo "${tblue}Enter the width: ${treset}"
-            read  width
-            mogrify -resize $width"x" $*
-            ;;
-        HEIGHT)
-          echo "${tyellow}Enter the width: ${treset}"
-            read  height
-            mogrify -resize x$height $*
-            ;;
-        FLOP)
-          mogrify -flop $*
-            ;;
-        EXIT)
-          exit 0
-            ;;
-        *)
-            echo "Invalid option"
-            break
-            ;;
+        echo -e "${tgreen}Size of the image${treset}"
+        du -sh $*
+        ;;
+      OPTIMIZE)
+        jpegoptim --strip-all --all-progressive -ptm 80 $*
+        break
+        ;;
+      WIDTH)
+        echo "${tblue}Enter the width: ${treset}"
+        read  width
+        mogrify -resize $width"x" $*
+        ;;
+      HEIGHT)
+        echo "${tyellow}Enter the width: ${treset}"
+        read  height
+        mogrify -resize x$height $*
+        ;;
+      FLOP)
+        mogrify -flop $*
+        break
+        ;;
+      ROTATE)
+        echo "${tblue}Enter the angle: ${treset}"
+        read  angle
+        mogrify -rotate $angle $*
+        break
+        ;;
+      EXIT)
+        exit 0
+        ;;
+      *)
+        echo "Invalid option"
+        break
+        ;;
     esac
-done
+  done
 }
 
 COLUMNS=1
