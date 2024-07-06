455c70c78e2ceb651e80b5a19560f53c4425ebf8
serii <seriiburduja@gmail.com>
2024-07-06 09:50:31 +0300

    feat: webm.sh

--- bash-scripts/webm.sh
+++ bash-scripts/webm.sh
@@ -1,25 +1,28 @@
 #! /bin/bash
 
-#a=1
-#for i in *; do
-#  #get extension
-#  ext="${i##*.}"
-#  new_file=$(echo "$a-music" | tr -d '\n').$ext
-#  mv -i -- "$i" "$new_file"
-#  a=`expr $a + 1`
-#done
+# Check if ffmpeg is installed
+if ! [ -x "$(command -v ffmpeg)" ]; then
+  echo 'Error: ffmpeg is not installed.' >&2
+  exit 1
+fi
 
-# ls *.webm | while read NAME; do
-#   ffmpeg -i "$NAME" "${NAME%.*}.mp3"
-# done
+mkdir mp3
 
-# for FILE in *.webm; do
-#     echo -e "Processing video '\e[32m$FILE\e[0m'";
-#     ffmpeg -i "${FILE}" -vn -ab 128k -ar 44100 -y "${FILE%.webm}.mp3";
-# done;
+## if has webm files
+if [ -f *.webm ]; then
+    for FILE in *.webm; do
+        echo -e "Processing video '\e[32m$FILE\e[0m'";
+        # ffmpeg -i "${FILE}" "${FILE%.webm}.mp3"; 
+        ffmpeg -i "${FILE}" -vn -ab 128k -ar 44100 -y "mp3/${FILE%.webm}.mp3";
+    done;
+fi
 
-for FILE in *.wav; do
-    echo -e "Processing video '\e[32m$FILE\e[0m'";
-    # ffmpeg -i "${FILE}" "${FILE%.webm}.mp3"; 
-    ffmpeg -i "${FILE}" -vn -ab 128k -ar 44100 -y "${FILE%.webm}.mp3";
-done;
+## if has wav files
+if [ -f *.wav ]; then
+    for FILE in *.wav; do
+        echo -e "Processing video '\e[32m$FILE\e[0m'";
+        # ffmpeg -i "${FILE}" "${FILE%.webm}.mp3"; 
+        ffmpeg -i "${FILE}" -vn -ab 128k -ar 44100 -y "mp3/${FILE%.webm}.mp3";
+    done;
+fi
+
