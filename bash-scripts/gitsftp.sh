629a3c381ef9c4ed0b27da7758b87178c16510b8
serii <seriiburduja@gmail.com>
2024-07-19 11:12:36 +0300

    upd: gitsft

--- bash-scripts/gitsftp.sh
+++ bash-scripts/gitsftp.sh
@@ -8,18 +8,13 @@ if [ -f $sftp_config_file_json ]; then
   remote_path=$(jq -r '.remote.path' $sftp_config_file_json)
   modified_files=$(git ls-files --modified)
   for file in $modified_files; do
-    echo "file: $file"
+    echo "${tblue}file: $file${treset}"
     full_path=$remote_path/$file
-    echo "full_path: $full_path"
+    echo "${tgreen}full_path: $full_path${treset}"
     rsync -avz -e ssh $file $user@$host:$full_path
   done
 else
-  echo "File $sftp_config_file_json does not exist."
-  echo "Please create a file $sftp_config_file_json with the following content:"
-  echo "{
-  \"host\": \"your_host\",
-  \"user\": \"your_user\",
-  \"remote_path\": \"your_remote_path\"
-}"
+  echo "${tmagenta}File $sftp_config_file_json does not exist.${treset}"
+  echo "${tyellow}Please create a file $sftp_config_file_json with the following content:${treset}"
   exit 1
 fi
