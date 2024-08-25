888ffa34d92ee0c227508f800f7190b0d39ab64b
Serii <seriiburduja@gmail.com>
2024-08-25 14:07:32 +0300

    feat: ignore mgitstatus

--- bash-scripts/ignore_mgitstatus.sh
+++ bash-scripts/ignore_mgitstatus.sh
@@ -1,26 +1,31 @@
-#!/bin/bash -x
+#!/bin/bash 
 user=$(whoami)
-clipboard=$(xclip -o)
+clipboard=$(xsel -b)
 file=~/Downloads/ignore_mgitstatus.txt
 touch $file
-echo $clipboard > ~/Downloads/ignore_mgitstatus.txt
+echo $clipboard > $file
+# after Untracked files add new line
+sed -i 's/Untracked files/Untracked files\n/g' $file
+# remove space at the beginning of the line
+sed -i 's/^ //g' $file
 # for each line in file remove ./ from the beginning
-sed -i 's/^\.\///g' ~/Downloads/ignore_mgitstatus.txt
-# choose file from downloads with fzf
-# loop throw file
+sed -i 's/^\.\///g' $file
+# # for each line remove : Untracked files at the end
+sed -i 's/: Untracked files//g' $file
+cd
 while IFS= read -r line
 do
   if [ -n "$line" ]; then
-    line="/home/${user}/${line}"
+    echo "line: $line"
+    line="${line}"
     if [ -d $line ]; then
       cd $line
       git config --local mgitstatus.ignore true
-      cd ~/Downloads
+      cd 
     else
       echo "Directory $line does not exist"
       exit
     fi
   fi
 done < $file
-
 rm $file
