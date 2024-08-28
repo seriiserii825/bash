2da046b7dc07ba21baa812254c8d3ef4cf98f3a0
serii burduja <seriiburduja@gmail.com>
2024-08-28 15:04:16 +0300

    feat: text-align

--- bash-scripts/scss.sh
+++ bash-scripts/scss.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+function convertToRem(){
+  scss_file=$1
+  while read -r line; do
+    if [[ $line == *"border:"* || $line == *"border-bottom:"* || $line == *"max-width"* || $line == *"linear-gradient"* || $line == *"&"* || $line == *"width: 0.1rem ;"* || $line == *"height: 1px;"* ]]; then
+      # echo "$line"
+      continue
+    else
+      # Use regular expressions to find pixel values (e.g., "10px", "20px", etc.)
+      px_values=$(echo "$line" | grep -oE "[0-9]+px")
+      new_line="$line"
+
+    # Iterate through each found pixel value
+    for px_value in $px_values; do
+      # Extract the numeric value from the pixel value
+      numeric_value=$(echo "$px_value" | grep -oE "[0-9]+")
+
+      # Convert the pixel value to rem and divide by 10
+      rem_value=$(awk "BEGIN { printf \"%.2f\", $numeric_value / 10 }")
+
+      # Replace the pixel value with the calculated rem value
+      new_line=$(echo "$new_line" | sed "s/$px_value/${rem_value}rem/g")
+
+      # echo "'new_line is:' $new_line"
+    done
+    # Print the modified line
+    # echo "$line"
+    # echo "$new_line"
+    sed -i "s/$line/$new_line/" $scss_file > /dev/null 2>&1
+    fi
+  done < "$scss_file"
+  yarn prettier --write $scss_file > /dev/null 2>&1
+}
 
 function scssHandler(){
   # sleep 1
@@ -41,6 +73,14 @@ function scssHandler(){
   line_with_color=$(grep -n "color" $scss_file | cut -d: -f2-)
   sed -i "/color/d" $scss_file
   echo $line_with_color >> $scss_file
+
+  ## line starts with text-align delete and move before color or line-height
+  line_with_text_align=$(grep -n "text-align" $scss_file | cut -d: -f2-)
+  sed -i "/text-align/d" $scss_file
+  # find line number of color or line-height
+  line_number=$(grep -n "color\|line-height" $scss_file | cut -d: -f1)
+  # insert text-align before color or line-height
+  sed -i "${line_number}i $line_with_text_align" $scss_file
 
   line_with_background=$(grep -n "background" $scss_file | cut -d: -f2-)
   sed -i "/background/d" $scss_file
