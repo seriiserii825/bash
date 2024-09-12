86fda0045a0fffd219191cbf2043ca3189833d00
Serii <seriiburduja@gmail.com>
2024-09-12 05:43:04 +0300

    feat: have a bug

--- bash-scripts/open_in_nvim.sh
+++ bash-scripts/open_in_nvim.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
 
-# Word to search for
-# search word is the content of the global clipboard
 search_word=$(xsel -b)
-echo $search_word
 
-# Find files containing the word, excluding __pycache__ and venv directories
-files=$(grep -rl --exclude-dir={__pycache__,venv} "$search_word" *)
-# echo $files
+echo "1) Search word from buffer and open in Neovim"
+echo "2) Find word in all files and another word not in the same file"
 
-# Open all files in a single Neovim instance
-if [ -n "$files" ]; then
-  nvim $files
+read -p "Enter your choice: " choice
+
+exclude_dirs="{__pycache__,venv}"
+
+if [ $choice -eq 1 ]; then
+  echo "Searching for word in all files and opening in Neovim"
+  files=$(grep -rl --exclude-dir=$exclude_dirs "$search_word" *)
+  echo "Files: $files"
+
+  # Open all files in a single Neovim instance
+  if [ -n "$files" ]; then
+    # nvim $files
+    echo $files
+  else
+    echo "No files found containing the word '$search_word'."
+  fi
+elif [ $choice -eq 2 ]; then
+#  grep -rl 'EC.presence_of_element_located' --exclude-dir=$exclude_dirs . | xargs grep -L 'from selenium.webdriver.support import expected_conditions as EC'
+ files=$(grep -rl 'EC.presence_of_element_located' --exclude-dir=$exclude_dirs . | xargs grep -L 'from selenium.webdriver.support import expected_conditions as EC')
+
+  # Open all files in a single Neovim instance
+  if [ -n "$files" ]; then
+    nvim $files
+  else
+    echo "No files found containing the word '$search_word'."
+  fi
 else
-  echo "No files found containing the word '$search_word'."
+  echo "Invalid choice"
+  exit 1
 fi
+
