dd9953d6c105e87133469e9c1fba4bd1b8e7e48d
serii burduja <seriiburduja@gmail.com>
2024-09-19 18:47:33 +0300

    feat: find word

--- bash-scripts/find_word.sh
+++ bash-scripts/find_word.sh
@@ -3,4 +3,8 @@
 read -p "Enter a word: " word
 read -p "Enter file_type: " file_type
 
-grep --color=always -Rnw . -e $word  --include=*.$file_type
+# exclude node_modules and .git
+# exclude dist/main-hash.js
+# exclude files with min.js
+grep --color=always -Rnw . -e $word --exclude-dir={node_modules,.git} --exclude=dist/main-*.js --exclude=*min.js --include=*.$file_type
+# grep --color=always -Rnw . -e $word  --include=*.$file_type
