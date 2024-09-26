6932a7cf24aeb52450e1dbcdce194c3b888c7128
serii burduja <seriiburduja@gmail.com>
2024-09-26 16:23:21 +0300

    feat: vimspector

---
+++ bash-scripts/vimspector.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+touch .vimspector.json
+
+cat <<EOF > .vimspector.json
+{
+  "configurations": {
+    "Python: Launch": {
+      "adapter": "debugpy",
+      "filetypes": [ "python" ],
+      "configuration": {
+        "name": "Python: Launch",
+        "type": "python",
+        "request": "launch",
+        "cwd": "\${workspaceRoot}",
+        "python": "./venv/bin/python",
+        "stopOnEntry": true,
+        "console": "externalTerminal",
+        "debugOptions": [],
+        "program": "\${file}"
+      }
+    }
+  }
+}
+
+EOF
+
