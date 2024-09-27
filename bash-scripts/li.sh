609e4226729f34dec56ea3d421d89ff54209dc95
serii burduja <seriiburduja@gmail.com>
2024-09-27 16:24:15 +0300

    upd: li.sh update

--- bash-scripts/li.sh
+++ bash-scripts/li.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-# Check if the correct number of arguments is provided
-cd ~/Downloads
-filename='list.txt'
-touch "$filename"
-xclip -o >> "$filename"
-new_file="new_file.txt"
-if [ -f "$new_file" ]; then
-    rm "$new_file"
-fi
-touch "$new_file"
+while /home/serii/Documents/bash/bash-scripts/clipnotify;
+do
+  sleep 2
+  output_file=~/Downloads/new_file.txt
+  touch $output_file
+  xsel --clipboard --output > "$output_file"
+  file_content=$(cat $output_file)
+  notify-send "$(echo -e "$file_content")" 
+  # loop thorw file and add <ul and li tags
+  new_value="<ul>\n"
+  while IFS= read -r line
+  do
+    new_value+="<li>$line</li>\n"
+  done < "$output_file"
+  new_value+="</ul>"
+  #remove empty lines
+  new_value=$(echo -e "$new_value" | sed '/^\s*$/d')
+  echo -n $new_value | xclip -selection clipboard
+  notify-send "$(echo -e "$new_value")" 
 
-echo "<ul>" >> "$new_file"
-while IFS= read -r line; do
-    echo "<li>${line}</li>" >> "$new_file"
-done < "$filename"
-echo "</ul>" >> "$new_file"
-
-cat "$new_file" > "$filename"
-
-xclip -sel clip < "$filename"
-rm "$filename"
-
+done
