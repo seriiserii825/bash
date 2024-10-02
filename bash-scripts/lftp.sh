f2c7b9615d4784809911ea9fa1091566f2b3a432
serii burduja <seriiburduja@gmail.com>
2024-10-02 18:51:59 +0300

    feat: lftp

--- bash-scripts/lftp.sh
+++ bash-scripts/lftp.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Read configuration from config.json
-CONFIG_FILE="config.json"
+CONFIG_FILE=".vscode/sftp.json"
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo "Configuration file not found: $CONFIG_FILE"
+  exit 1
+fi
 NAME=$(jq -r '.name' "$CONFIG_FILE")
 HOST=$(jq -r '.host' "$CONFIG_FILE")
 PROTOCOL=$(jq -r '.protocol' "$CONFIG_FILE")
@@ -9,6 +13,7 @@ PORT=$(jq -r '.port' "$CONFIG_FILE")
 USERNAME=$(jq -r '.username' "$CONFIG_FILE")
 PASSWORD=$(jq -r '.password' "$CONFIG_FILE")
 REMOTE_PATH=$(jq -r '.remotePath' "$CONFIG_FILE")
+REMOTE_PATH="$REMOTE_PATH/"
 WATCH_FILES=$(jq -r '.watcher.files' "$CONFIG_FILE")
 IGNORE_PATTERNS=$(jq -r '.ignore | join("|")' "$CONFIG_FILE")
 
@@ -16,42 +21,22 @@ IGNORE_PATTERNS=$(jq -r '.ignore | join("|")' "$CONFIG_FILE")
 upload_file() {
   local file_path=$1
   local relative_path=${file_path#./}  # Get the relative path from the current directory
-  echo "Uploading $file_path to $REMOTE_PATH$relative_path"
+  # echo "Uploading $file_path to $REMOTE_PATH$relative_path"
   notify-send "Uploading $file_path to $REMOTE_PATH$relative_path"
-
-  # Use lftp with mkdir -p to ensure the directory structure is created
-  lftp -u "$USERNAME","$PASSWORD" -e "
-    set ssl:verify-certificate no;
-    mkdir -p $REMOTE_PATH$(dirname "$relative_path");  # Ensure directory exists
-    put $file_path -o $REMOTE_PATH$relative_path;  # Upload the file
-    bye
-  " "$PROTOCOL://$HOST:$PORT"
-}
-
-# Function to delete a file from the remote server using lftp
-delete_file() {
-  local file_path=$1
-  local relative_path=${file_path#./}  # Get the relative path from the current directory
-  echo "Deleting $file_path from $REMOTE_PATH$relative_path"
-  notify-send "Deleting $file_path from $REMOTE_PATH$relative_path"
-  
-  # Use lftp to delete the file from the server
-  lftp -u "$USERNAME","$PASSWORD" -e "
-    set ssl:verify-certificate no;
-    rm $REMOTE_PATH$relative_path;  # Delete the file
-    bye
-  " "$PROTOCOL://$HOST:$PORT"
+  lftp -u "$USERNAME","$PASSWORD" -e "set ssl:verify-certificate no; mkdir -p $REMOTE_PATH$(dirname "$relative_path"); put $file_path -o $REMOTE_PATH$relative_path; bye" "$PROTOCOL://$HOST:$PORT"
 }
 
 # Start watching files with inotify
 inotifywait -m -r -e modify,create,delete --exclude "$IGNORE_PATTERNS" --format "%w%f %e" . | while read file event; do
   if [[ $event == *DELETE* ]]; then
-    delete_file "$file"
+    local relative_path=${file#./}  # Get the relative path from the current directory
+    # echo "Deleting $file from $REMOTE_PATH$relative_path"
+    # notify-send "Deleting $file from $REMOTE_PATH$relative_path"
+    lftp -u "$USERNAME","$PASSWORD" -e "rm $REMOTE_PATH$relative_path; bye" "$PROTOCOL://$HOST:$PORT"
   else
-    echo "Detected $event on $file"
-    notify-send "Detected $event on $file"
+    # echo "Detected $event on $file"
+    # notify-send "Detected $event on $file"
     # Sleep for 2 seconds to wait for compilation to finish
-    sleep 2
     upload_file "$file"
   fi
 done
--- bash-scripts/rsync.sh
+++ bash-scripts/rsync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Read configuration from config.json
-CONFIG_FILE="config.json"
+CONFIG_FILE=".vscode/sftp.json"
 NAME=$(jq -r '.name' "$CONFIG_FILE")
 HOST=$(jq -r '.host' "$CONFIG_FILE")
 PROTOCOL=$(jq -r '.protocol' "$CONFIG_FILE")
@@ -10,17 +10,27 @@ USERNAME=$(jq -r '.username' "$CONFIG_FILE")
 PASSWORD=$(jq -r '.password' "$CONFIG_FILE")
 REMOTE_PATH=$(jq -r '.remotePath' "$CONFIG_FILE")
 REMOTE_PATH="$REMOTE_PATH/"
-WATCH_FILES=$(jq -r '.watcher.files' "$CONFIG_FILE")
 IGNORE_PATTERNS=$(jq -r '.ignore | join("|")' "$CONFIG_FILE")
+
+
+# echo "Starting $NAME"
+# echo "Host: $HOST"
+# echo "Protocol: $PROTOCOL"
+# echo "Port: $PORT"
+# echo "Username: $USERNAME"
+# echo "Password: $PASSWORD"
+# echo "Remote Path: $REMOTE_PATH"
+# echo "Ignore Patterns: $IGNORE_PATTERNS"
 
 # Function to upload a file using rsync with sshpass
 upload_file() {
   local file_path=$1
   local relative_path=${file_path#./}  # Get the relative path from the current directory
-  echo "Uploading $file_path to $REMOTE_PATH$relative_path"
-  notify-send "Uploading $file_path to $REMOTE_PATH$relative_path"
 
   sshpass -p "$PASSWORD" rsync -avz --progress --rsh="sshpass -p $PASSWORD ssh -p $PORT" "$file_path" "$USERNAME@$HOST:$REMOTE_PATH$relative_path"
+
+  echo "Uploading $file_path to $REMOTE_PATH$relative_path"
+  notify-send "Uploading $file_path to $REMOTE_PATH$relative_path"
 }
 
 # Function to delete a file from the remote server using ssh and sshpass
@@ -28,10 +38,10 @@ delete_file() {
   local file_path=$1
   local relative_path=${file_path#./}  # Get the relative path from the current directory
 
-  echo "Deleting $file_path from $REMOTE_PATH$relative_path"
-  # notify-send "Deleting $file_path from $REMOTE_PATH$relative_path"
-
   sshpass -p "$PASSWORD" ssh -p "$PORT" "$USERNAME@$HOST" "rm -f $REMOTE_PATH$relative_path"
+
+  echo "Deleting $file_path from $REMOTE_PATH$relative_path"
+  notify-send "Deleting $file_path from $REMOTE_PATH$relative_path"
 }
 
 # Start watching files with inotify
