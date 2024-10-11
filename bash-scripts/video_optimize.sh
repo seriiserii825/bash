375be4714f14492cd0c8b5eb168fa94cccf0a979
serii burduja <seriiburduja@gmail.com>
2024-10-11 10:57:58 +0300

    feat: video optimize

--- bash-scripts/video_optimize.sh
+++ bash-scripts/video_optimize.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# get all mp4 in folder and convert to mp3
-for i in *.mp4; do
-    # ffmpeg -i "$i" -vn -acodec libmp3lame -ac 2 -ab 160k -ar 48000 "${i%.*}.mp3"
-    optimized_name="${i%.*}_optimized.mp4"
-    ffmpeg -i "$i" -vcodec libx264 -crf 28 -preset slow -acodec aac -b:a 128k $optimized_name
-done
+choosed_video_with_fzf=$(find . -maxdepth 1 -type f -name "*.mp4" | fzf)
+
+read -p "Enter quality (18-28), 18 best : " quality
+if [ -z "$quality" ]; then
+    quality=20
+fi
+
+# optimized_name="${i%.*}_optimized_${quality}.mp4"
+optimized_name="${choosed_video_with_fzf%.*}_optimized_${quality}.mp4"
+ffmpeg -i "$choosed_video_with_fzf" -vcodec libx264 -crf $quality -preset slow -acodec aac -b:a 128k $optimized_name
