472680cbfd0070e800f2dbd4909e8c2cf3e5c525
serii burduja <seriiburduja@gmail.com>
2024-10-16 10:36:16 +0300

    upd: magick version

--- bash-scripts/webp_to_jpg.sh
+++ bash-scripts/webp_to_jpg.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Check if magick is installed 
+if ! [ -x "$(command -v magick)" ]; then
+  echo 'Error: magick is not installed.' >&2
+  exit 1
+fi
+
+# check magick version is greater than 7
+if [ "$(magick -version | grep -oP '(?<=Version: ImageMagick )\d+')" -lt 7 ]; then
+  echo 'Error: magick version is less than 7.' >&2
+  exit 1
+fi
+
 # check if web files exist
 if [ ! -f *.webp ]; then
   echo "No .webp files found in the current directory."
