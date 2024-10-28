abd7aaabbb3b158ba731bbca99df5b451b13feb3
serii burduja <seriiburduja@gmail.com>
2024-10-28 18:38:39 +0200

    feat: treesitter script

--- bash-scripts/treesitter.sh
+++ bash-scripts/treesitter.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+# line = "source \$HOME/.config/nvim/modules/treesitter.vim"
+# file = "~/.config/nvim/init.vim"
 
-#!/bin/bash
-
-if grep -q -F "#source \$HOME/.config/nvim/modules/treesitter.vim" ~/.config/nvim/init.vim; then
+if grep -q -F "\"source \$HOME/.config/nvim/modules/treesitter.vim" ~/.config/nvim/init.vim; then
   echo "to uncomment"
   #comment line
-  sed -i 's/#source \$HOME\/.config\/nvim\/modules\/treesitter.vim/source \$HOME\/.config\/nvim\/modules\/treesitter.vim/g' ~/.config/nvim/init.vim
+  sed -i 's/\"source \$HOME\/.config\/nvim\/modules\/treesitter.vim/source \$HOME\/.config\/nvim\/modules\/treesitter.vim/g' ~/.config/nvim/init.vim
 elif grep -q -F "source \$HOME/.config/nvim/modules/treesitter.vim" ~/.config/nvim/init.vim; then
   echo "to comment"
   #uncomment line
-  sed -i 's/source \$HOME\/.config\/nvim\/modules\/treesitter.vim/#source \$HOME\/.config\/nvim\/modules\/treesitter.vim/g' ~/.config/nvim/init.vim
+  sed -i 's/source \$HOME\/.config\/nvim\/modules\/treesitter.vim/\"source \$HOME\/.config\/nvim\/modules\/treesitter.vim/g' ~/.config/nvim/init.vim
 fi
