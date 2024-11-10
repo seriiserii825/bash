8180d12e136760f491d5583a861771b20da88c65
serii burduja <seriiburduja@gmail.com>
2024-11-10 18:00:17 +0200

    feat: docker env

--- bash-scripts/docker.sh
+++ bash-scripts/docker.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 file_name="docker-compose.yaml"
+env_example_file=".env.example"
 if [ ! -f "$file_name" ]; then
     echo "${tmagenta}File $file_name does not exist.${treset}"
     exit 1
@@ -9,5 +10,6 @@ fi
 read -p "Enter the name of the docker: " docker_name
 
 sed -i "s/course/$docker_name/g" $file_name
+sed -i "s/course/$docker_name/g" $env_example_file
 
 echo "${tgreen}Docker name changed to $docker_name${treset}"
