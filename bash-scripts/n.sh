a47e8c461330db1d4d38bfba0ed9a3de4b120815
serii burduja <seriiburduja@gmail.com>
2025-01-13 10:50:54 +0200

    feat: n.sh update

--- bash-scripts/n.sh
+++ bash-scripts/n.sh
@@ -3,14 +3,16 @@
 # toggle comment string source ~/Documents/bash-scripts/zsh_nvm in .zshrc
 
 # check if .zshrc has string source ~/Documents/bash-scripts/zsh_nvm
+script_path=~/Documents/bash/bash-scripts/zsh_nvm
+escaped_path=$(echo $script_path | sed 's/\//\\\//g')
 
-if grep -q "^source ~/Documents/bash/bash-scripts/zsh_nvm" ~/.zshrc; then
+if grep -q "^source $script_path" ~/.zshrc; then
   #comment this string
-  sed -i 's/^source ~\/Documents\/bash\/bash-scripts\/zsh_nvm/#source ~\/Documents\/bash\/bash-scripts\/zsh_nvm/g' ~/xubuntu/.zshrc
+  sed -i "s/^source $escaped_path/#source $escaped_path/g" ~/xubuntu/.zshrc
   echo "zsh_nvm commented"
   exec zsh
 else
-  sed -i 's/^#source ~\/Documents\/bash\/bash-scripts\/zsh_nvm/source ~\/Documents\/bash\/bash-scripts\/zsh_nvm/g' ~/xubuntu/.zshrc
+  sed -i "s/^#source $escaped_path/source $escaped_path/g" ~/xubuntu/.zshrc
   echo "zsh_nvm uncommented"
   exec zsh
 fi
