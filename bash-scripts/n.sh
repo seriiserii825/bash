60ad1c215c9e8fe8eb5395ec5daae5d98103c751
serii burduja <seriiburduja@gmail.com>
2025-01-13 11:13:05 +0200

    feat: update n.sh and nvm_zsh

--- bash-scripts/n.sh
+++ bash-scripts/n.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-# toggle comment string source ~/Documents/bash-scripts/zsh_nvm in .zshrc
+# Path to the .zshrc file
+zshrc_file="$HOME/.zshrc"
 
-# check if .zshrc has string source ~/Documents/bash-scripts/zsh_nvm
-script_path=~/Documents/bash/bash-scripts/zsh_nvm
-escaped_path=$(echo $script_path | sed 's/\//\\\//g')
+# Define the script path
+script_path="~/Documents/bash/bash-scripts/zsh_nvm"
 
-if grep -q "^source $script_path" ~/.zshrc; then
-  #comment this string
-  sed -i "s/^source $escaped_path/#source $escaped_path/g" ~/xubuntu/.zshrc
-  echo "zsh_nvm commented"
-  exec zsh
+# Escape the script path for sed
+escaped_path=$(echo "$script_path" | sed 's/\//\\\//g')
+
+# Check if the line is uncommented
+if grep -q "^source $script_path" "$zshrc_file"; then
+  # Comment the line
+  sed -i "s|^source $escaped_path|#source $escaped_path|" "$zshrc_file"
+  echo "The line has been commented."
 else
-  sed -i "s/^#source $escaped_path/source $escaped_path/g" ~/xubuntu/.zshrc
-  echo "zsh_nvm uncommented"
-  exec zsh
+  # Uncomment the line
+  sed -i "s|^#source $escaped_path|source $escaped_path|" "$zshrc_file"
+  echo "The line has been uncommented."
 fi
+
+# Reload zsh
+exec zsh
