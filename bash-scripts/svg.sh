6bdc012b19f2369c9789743ad3f61a29b1dc8c4f
serii burduja <seriiburduja@gmail.com>
2025-01-23 17:35:08 +0200

    feat: svg optimize

--- bash-scripts/svg.sh
+++ bash-scripts/svg.sh
@@ -1,14 +1,27 @@
 #! /bin/bash
 
 file_path=$( fzf )
-read -p "Enter width: " width
-file=$( basename "$file_path" .svg )
-new_file=$( echo "$file"-"$width".svg )
 
-rsvg-convert -a -w "$width" -f svg "$file".svg -o "$file"-"$width".svg
-svgo "$file"-"$width".svg
+read -p "Enter w/o(width,optimize): " choose
 
-cat "$file"-"$width".svg | xclip -selection clipboard
-echo "${tgreen}your file is ready: $file-$width.svg${treset}"
-bat "$file-$width.svg"
-echo "${tblue}Copied to clipboard!${treset}"
+if [ "$choose" == "w" ]; then
+    read -p "Enter width: " width
+    file=$( basename "$file_path" .svg )
+    new_file=$( echo "$file"-"$width".svg )
+
+    rsvg-convert -w "$width" -f svg "$file".svg -o "$file"-"$width".svg
+    svgo "$file"-"$width".svg
+
+    cat "$file"-"$width".svg | xclip -selection clipboard
+    echo "${tgreen}your file is ready: $file-$width.svg${treset}"
+    bat "$file-$width.svg"
+    echo "${tblue}Copied to clipboard!${treset}"
+    exit
+  else
+    # svgo file -o file_output
+    # change output file with posfix optimize
+    file_name=$( basename "$file_path" .svg )
+    new_file=$( echo "$file_name"-optimize.svg )
+    svgo "$file_path" -o "$new_file"
+    bat "$new_file"
+fi
