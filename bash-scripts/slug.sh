c62ae317aa723f96dbf0f314ef4a3c33ab46e4c1
serii burduja <seriiburduja@gmail.com>
2025-02-02 13:30:12 +0200

    feat: update slug.sh

--- bash-scripts/slug.sh
+++ bash-scripts/slug.sh
@@ -5,16 +5,27 @@ echo "${tblue}Copy text with non-breaking spaces in clipboard and wait for the s
 echo "${tyellow}Use text with non-breaking spaces in clipboard${treset}"
 echo "${tmagenta}Press Ctrl+C to stop the script${treset}"
 
-while /home/serii/Documents/bash/bash-scripts/clipnotify;
-do
+# while /home/serii/Documents/bash/bash-scripts/clipnotify;
+# do
+#   sleep 2
+#   echo $clipboard
+#   clipboard=$(xclip -o -selection clipboard)
+#   notify-send "$(echo -e "$clipboard")"
+#   lower_clipboard=$(echo $clipboard | tr '[:upper:]' '[:lower:]')
+#   slug_clipboard=$(echo $lower_clipboard | tr ' ' '-')
+#   # remove /()[]{}<>?| from slug
+#   slug_clipboard=$(echo $slug_clipboard | tr -d '/()[]{}<>?|’')
+#   xclip -selection clipboard -t text/plain -i <<< "$slug_clipboard"
+#   notify-send "$(echo -e "$slug_clipboard")" 
+# done
+#
+#
+while /home/serii/Documents/bash/bash-scripts/clipnotify; do
   sleep 2
-  echo $clipboard
   clipboard=$(xclip -o -selection clipboard)
-  notify-send "$(echo -e "$clipboard")"
-  lower_clipboard=$(echo $clipboard | tr '[:upper:]' '[:lower:]')
-  slug_clipboard=$(echo $lower_clipboard | tr ' ' '-')
-  # remove /()[]{}<>?| from slug
-  slug_clipboard=$(echo $slug_clipboard | tr -d '/()[]{}<>?|’')
+  echo "$clipboard"
+  notify-send "$clipboard"
+  slug_clipboard=$(echo "$clipboard" | tr '[:upper:]' '[:lower:]' | tr ' ' '-' | tr -d '/()[]{}<>?|’')
   xclip -selection clipboard -t text/plain -i <<< "$slug_clipboard"
-  notify-send "$(echo -e "$slug_clipboard")" 
+  notify-send "$slug_clipboard"
 done
