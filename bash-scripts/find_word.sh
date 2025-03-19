432e0e0f27c9268ff7b55ec01e207fd89bbaed5a
serii burduja <seriiburduja@gmail.com>
2025-03-19 11:21:20 +0200

    feat: find word version 1

--- bash-scripts/find_word.sh
+++ bash-scripts/find_word.sh
@@ -1,10 +1,46 @@
-#!/bin/bash
+#!/bin/bash -x
 
-read -p "Enter a word: " word
-read -p "Enter file_type: " file_type
+echo -n "Word to find: "
+read -r WORD
 
-# exclude node_modules and .git
-# exclude dist/main-hash.js
-# exclude files with min.js
-grep --color=always -Rnw . -e $word --exclude-dir={node_modules,.git} --exclude=dist/main-*.js --exclude=*min.js --include=*.$file_type
-# grep --color=always -Rnw . -e $word  --include=*.$file_type
+# Initialize ignore flags
+ignore_file=""
+ignore_dir=""
+
+# File ignoring
+echo -n "Do you want to ignore a specific file? (y/n): "
+read -r ASK_IGNORE_FILE
+if [[ "$ASK_IGNORE_FILE" == "y" ]]; then
+    file_path=$(fzf)
+    if [[ -n "$file_path" ]]; then
+        ignore_file="--ignore-file=is:$(basename "$file_path")"
+    fi
+fi
+
+# Directory ignoring
+echo -n "Do you want to ignore a directory? (y/n): "
+read -r ASK_IGNORE_DIR
+if [[ "$ASK_IGNORE_DIR" == "y" ]]; then
+    dir_path=$(fzf)
+    # get directory path from dir_path that it's a file path
+    if [[ -f "$dir_path" ]]; then
+        dir_path=$(dirname "$dir_path")
+    fi
+    
+    if [[ -n "$dir_path" ]]; then
+        ignore_dir="--ignore-dir=$(basename "$dir_path")"
+    fi
+fi
+
+# Build command
+cmd="ack \"$WORD\""
+if [[ -n "$ignore_file" ]]; then
+    cmd+=" $ignore_file"
+fi
+if [[ -n "$ignore_dir" ]]; then
+    cmd+=" $ignore_dir"
+fi
+
+# Execute
+echo "Running: $cmd"
+eval "$cmd"
