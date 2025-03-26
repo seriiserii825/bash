d775465023bed1f847362e1b71b09388d7ecad95
serii burduja <seriiburduja@gmail.com>
2025-03-26 13:00:17 +0200

    feat: kill port

--- bash-scripts/kill_port.sh
+++ bash-scripts/kill_port.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 read -p "Enter port number: " port
+sudo netstat -tulnp | grep ':$port'
 fuser -k $port/tcp && echo 'Terminated' || echo "Nothing was running on the $port"
 # fuser -k $port_number/tcp && echo 'Terminated' || echo "Nothing was running on the $port_number"
