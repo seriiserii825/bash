3f2a9b55ddfcd56d077e0f6154651429d7770b70
serii burduja <seriiburduja@gmail.com>
2025-04-12 22:00:29 +0300

    feat: wmcrtl

---
+++ bash-scripts/fontawesome.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Find icon by argument after calling script
+# Example: ./fontawesome.sh setting
+# Open browser with search result for "setting" icon
+
+chrome_path=$(which google-chrome-stable)
+if [ -z "$chrome_path" ]; then
+  echo "Google Chrome is not installed."
+  exit 1
+fi
+
+icon_name=$1
+if [ -z "$icon_name" ]; then
+  echo "Please provide an icon name."
+  exit 1
+fi
+
+icon_name=$(echo "$icon_name" | tr '[:upper:]' '[:lower:]')
+
+# Open browser with search result for the icon
+$chrome_path "https://fontawesome.com/search?q=$icon_name&o=r&ic=free" &
+
+# Wait for the browser to fully load the window
+sleep 2
+
+# Focus the browser window using wmctrl
+wmctrl -x -a "google-chrome.Google-chrome"
