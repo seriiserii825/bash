3d3a626f3a9f448eca959bb5008d2811995352c9
serii burduja <seriiburduja@gmail.com>
2025-04-25 10:34:20 +0300

    feat: rename files

--- bash-scripts/rename_files.sh
+++ bash-scripts/rename_files.sh
@@ -1,13 +1,14 @@
 #! /bin/bash 
 
 read -p "Enter FileName: " filename
+read -p "New FileName: " newfilename
 
-find . -type f -name "*$filename*" | sed "p;s/$filename/Home/" 
+find . -type f -name "*$filename*" | sed "p;s/$filename/$newfilename/"
 
 read -p "Do you want to rename the files? (y/n): " answer
 if [[ $answer == "y" ]]; then
-    find . -type f -name '*Hero*' | while read -r file; do
-    newname="$(dirname "$file")/$(basename "$file" | sed 's/Hero/Home/g')"
+  find . -type f -name "*$filename*" | while read -r file; do
+    newname="$(dirname "$file")/$(basename "$file" | sed "s/$filename/$newfilename/g")"
     mv "$file" "$newname"
   done
 echo "Files renamed successfully."
