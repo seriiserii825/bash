0ba145ebe8d623858dc4e32ddfad92b96f7714e6
serii burduja <seriiburduja@gmail.com>
2025-05-03 07:51:25 +0300

    feat: lf.sh again

--- bash-git/lf.sh
+++ bash-git/lf.sh
@@ -1,33 +1,32 @@
-gitMenu(){
-  script_dir="$HOME/Documents/bash/bash-git"
-  source "$script_dir/git-push.sh"
-  source "$script_dir/git-clone.sh"
+#!/usr/bin/bash 
 
-  menu_items=(
-    "Push"
-    "Pull"
-    "Sync"
-    "Clone"
-    )
-  # choose with fzf
-  selected_item=$(printf '%s\n' "${menu_items[@]}" | fzf --height 40% --reverse --inline-info --prompt "Select an option: ")
+source "$(dirname "$0")/git-push.sh"
+source "$(dirname "$0")/git-clone.sh"
 
-  if [[ "$selected_item" == "Push" ]]; then
-    echo "${tmagenta}Pushing...${treset}"
-    gitPush 
-  elif [[ "$selected_item" == "Pull" ]]; then
-    echo "${tmagenta}Pulling...${treset}"
-  elif [[ "$selected_item" == "Sync" ]]; then
-    echo "${tmagenta}Syncing...${treset}"
-    # # Sync
-    # git add .
-    # git commit -m "$message"
-    # git pull --rebase
-    # git push
-  elif [[ "$selected_item" == "Clone" ]]; then
-    echo "${tmagenta}Cloning...${treset}"
-    gitClone
-  else
-    echo "${tmagenta}Invalid option selected. Exiting...${treset}"
-  fi
-}
+menu_items=(
+  "Push"
+  "Pull"
+  "Sync"
+  "Clone"
+  )
+# choose with fzf
+selected_item=$(printf '%s\n' "${menu_items[@]}" | fzf --height 40% --reverse --inline-info --prompt "Select an option: ")
+
+if [[ "$selected_item" == "Push" ]]; then
+  echo "${tmagenta}Pushing...${treset}"
+  gitPush 
+elif [[ "$selected_item" == "Pull" ]]; then
+  echo "${tmagenta}Pulling...${treset}"
+elif [[ "$selected_item" == "Sync" ]]; then
+  echo "${tmagenta}Syncing...${treset}"
+  # # Sync
+  # git add .
+  # git commit -m "$message"
+  # git pull --rebase
+  # git push
+elif [[ "$selected_item" == "Clone" ]]; then
+  echo "${tmagenta}Cloning...${treset}"
+  gitClone
+else
+  echo "${tmagenta}Invalid option selected. Exiting...${treset}"
+fi
