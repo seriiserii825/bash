6e30a64e039c3ec90a1358f8ff65f6a77a3a9232
serii burduja <seriiburduja@gmail.com>
2025-05-05 18:50:34 +0300

    tx changed

---
+++ bash-scripts/weather.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+URL='http://www.accuweather.com/en/de/berlin/10178/weather-forecast/178087'
+
+wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $2": "$16", "$12"°" }'| head -1
