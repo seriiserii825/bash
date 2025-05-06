5e2257b379fcd768f83c253af45d4fd8c72749c5
serii burduja <seriiburduja@gmail.com>
2025-05-06 10:01:41 +0300

    upd: update

--- bash-git/lf.sh
+++ bash-git/lf.sh
@@ -1,10 +1,22 @@
 lf(){
-  message="$*"
   script_dir="$HOME/Documents/bash/bash-git"
   source "$script_dir/git-push.sh"
   source "$script_dir/git-pull.sh"
   source "$script_dir/git-clone.sh"
   source "$script_dir/git-sync.sh"
+
+  if [[ $# -eq 1 && $1 == '-h' ]]; then
+    echo "Usage: lf [message]"
+    echo "Push, pull, sync, or clone a git repository."
+    echo
+    echo "Options:"
+    echo "  message   The commit message to use when pushing."
+    echo
+    echo "Examples:"
+    echo "  lf 'Initial commit'"
+    echo "  lf -h"
+    return
+  fi
 
   menu_items=(
     "Push"
@@ -17,6 +29,11 @@ lf(){
 
   if [[ "$selected_item" == "Push" ]]; then
     echo "${tmagenta}Pushing...${treset}"
+    message="$*"
+    if [[ -z "$message" ]]; then
+      echo "No commit message provided. Exiting..."
+      return
+    fi
     gitPush $script_dir "$message"
   elif [[ "$selected_item" == "Pull" ]]; then
     echo "${tmagenta}Pulling...${treset}"
