ea93eb348531e7ab9d0d6d01055c5bfb87d02b67
serii burduja <seriiburduja@gmail.com>
2025-05-07 16:27:00 +0300

    feat: update video_optimize

--- bash-scripts/video_optimize.sh
+++ bash-scripts/video_optimize.sh
@@ -2,11 +2,21 @@
 
 choosed_video_with_fzf=$(find . -maxdepth 1 -type f -name "*.mp4" | fzf)
 
+if [ -z "$choosed_video_with_fzf" ]; then
+    echo "No video selected."
+    exit 1
+fi
+
 read -p "Enter quality (18-28), 18 best : " quality
-if [ -z "$quality" ]; then
+if [[ ! "$quality" =~ ^[0-9]+$ ]] || [ "$quality" -lt 18 ] || [ "$quality" -gt 28 ]; then
+    echo "Invalid input. Using default value: 20"
     quality=20
 fi
 
-# optimized_name="${i%.*}_optimized_${quality}.mp4"
-optimized_name="${choosed_video_with_fzf%.*}_optimized_${quality}.mp4"
-ffmpeg -i "$choosed_video_with_fzf" -vcodec libx264 -crf $quality -preset slow -acodec aac -b:a 128k $optimized_name
+output_dir="./optimized"
+mkdir -p "$output_dir"
+optimized_name="$output_dir/$(basename "${choosed_video_with_fzf%.*}_optimized_${quality}.mp4")"
+
+ffmpeg -i "$choosed_video_with_fzf" -vcodec libx264 -crf "$quality" -preset slow -acodec aac -b:a 128k "$optimized_name"
+
+echo "✅ Optimization complete: $optimized_name"
