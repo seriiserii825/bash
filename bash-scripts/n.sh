4f1f0053cf2874bbc498457f03b0b516569f86a1
serii burduja <seriiburduja@gmail.com>
2025-05-09 09:54:25 +0300

    feat: n.sh

---
+++ bash-scripts/n.sh
@@ -0,0 +1,83 @@
+checkNode(){
+  file="package.json"
+
+  # check if package.json exists
+  if [ ! -e "$file" ]; then
+    echo "No $file"
+    return
+  fi
+
+  # find in package.json line with "node"
+  node_line=$(grep "\"node\"" "$file")
+
+  if [ -z "$node_line" ]; then
+    echo "No node line in $file"
+    return
+  fi
+
+  # extract version (remove quotes and spaces)
+  node_version=$(echo "$node_line" | cut -d':' -f2 | tr -d '[:space:]' | tr -d '"')
+
+  if [ -z "$node_version" ]; then
+    echo "No node version in $file"
+    return
+  fi
+
+  # if version starts with ^
+  if [[ $node_version == ^* ]]; then
+    echo "Node version has ^"
+    node_version=${node_version#^}
+  fi
+
+  echo "Node version: $node_version"
+
+  # check current node version
+  current_version=$(node -v | tr -d 'v')
+
+  # check if current version is equal to node version
+  if [[ $current_version == $node_version ]]; then
+    echo "Node version is up to date"
+    return
+  else
+    export NVM_DIR="$HOME/.config/nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+    # Check if node version is already installed
+    if ! nvm ls "$node_version" | grep -q "$node_version"; then
+      echo "Node version $node_version not found. Installing..."
+      nvm install "$node_version" || {
+        echo "Failed to install Node version $node_version"
+              return
+            }
+    fi
+
+    # check if yarn is installed
+    if ! command -v yarn &> /dev/null; then
+        echo "Failed to install Yarn" return
+    fi
+
+    # Use the version
+    nvm use "$node_version" || {
+      echo "Failed to switch to Node version $node_version"
+          return
+        }
+  fi
+}
+
+yd(){
+  checkNode
+  yarn
+}
+yyd(){
+  checkNode
+  yarn
+}
+
+ybuild(){
+  checkNode
+  yarn build
+}
+yybuild(){
+  checkNode
+  yarn build
+}
