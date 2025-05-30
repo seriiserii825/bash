59fd4f080857a7002e52fd2ea94de6f82235b971
serii burduja <seriiburduja@gmail.com>
2025-05-30 22:26:07 +0300

    feat: update file.sh break

--- bash-scripts/file.sh
+++ bash-scripts/file.sh
@@ -18,24 +18,28 @@ function fromFile(){
         rm $file_path
         echo "${tgreen}File $file_path was deleted${treset}"
         ls -la
-        ;;
+        break
+				;;
       "${tmagenta}DeleteAllExceptThis${treset}")
         find . -maxdepth 1 -type f -not -name "$file" -delete
         echo "${tgreen}All files except $file were deleted${treset}"
         ls -la
-        ;;
+        break
+				;;
       "${tmagenta}DeleteAllWithPattern${treset}")
         read -p "Enter pattern to keep: " pattern
         find . -maxdepth 1 -type f -name "*$pattern*" -exec rm {} +
         echo "${tgreen}All files with $pattern were deleted${treset}"
         ls -la
-        ;;
+        break
+				;;
       "${tmagenta}DeleteAllExceptPattern${treset}")
         read -p "Enter pattern to keep: " pattern
         find . -maxdepth 1 -type f ! -name "*$pattern*" -exec rm {} +
         echo "${tgreen}All files except $pattern were deleted${treset}"
         ls -la
-        ;;
+        break
+				;;
       "${tgreen}Rename${treset}")
         read -p "Enter new file name: " new_file_name
         echo "file path is: $file_path"
@@ -43,44 +47,54 @@ function fromFile(){
         echo "dir path is: $dir_path"
         mv $file_path $dir_path/$new_file_name
         echo "${tgreen}File $file_path was renamed to $dir_path/$new_file_name${treset}"
-        ;;
+        break
+				;;
       "${tgreen}Read${treset}")
         bat $file_path
-        ;;
+        break
+				;;
       "${tblue}CopyName${treset}")
         echo $file_path | awk 'BEGIN{FS="/"}{print $NF}' | tr -d '\n' | xsel -b -i
         file_name=$( echo $file_path | awk 'BEGIN{FS="/"}{print $NF}' | tr -d '\n')
         echo "${tgreen}${file_name} was copied to clipboard${treset}"
-        ;;
+        break
+				;;
       "${tblue}CopyPath${treset}")
         echo $file_path | tr -d '\n' | xsel -b -i
         file_path=$( echo $file_path | tr -d '\n' )
         echo "${tgreen}Path ${file_path} was copied to clipboard${treset}"
-        ;;
+        break
+				;;
       "${tblue}CopyAbsPath${treset}")
         echo $abs_path | tr -d '\n' | xsel -b -i
         abs_full_path=$( echo $abs_path | tr -d '\n' )
         echo "${tgreen}Path ${abs_full_path} was copied to clipboard${treset}"
-        ;;
+        break
+				;;
       "${tblue}CopyToDownloads${treset}")
         cp $file_path ~/Downloads
         echo "${tgreen}File ${file_path} was copied to Downloads${treset}"
-        ;;
+        break
+				;;
       "${tgreen}Execute${treset}")
         chmod +x $file_path
         echo "${tgreen}"Execute" permission was added to $file_path${treset}"
-        ;;
+        break
+				;;
       "${tblue}FileToBuffer${treset}")
         cat $file_path | tr -d '\n' | xsel -b -i
         echo "${tgreen}File $file_path was copied to clipboard${treset}"
-        ;;
+        break
+				;;
       "${tblue}BufferToFile${treset}")
         clipboard=$(xclip -o -selection clipboard)
         echo $clipboard > $file_path
-        ;;
+        break
+				;;
       "${tblue}OpenInBrowser${treset}")
         google-chrome-stable $file_path
-        ;;
+        break
+				;;
       "${tblue}Multiply${treset}")
         file_name=$( echo $file_path | awk 'BEGIN{FS="/"}{print $NF}' | tr -d '\n' )
         file_without_extension="${file_path%.*}"
@@ -94,7 +108,8 @@ function fromFile(){
         for i in $( seq 1 $multiplier ); do
           cp $file_path $file_without_extension-$i.$file_extension
         done
-        ;;
+        break
+				;;
       "${tblue}Find words${treset}")
         file_name=$( echo $file_path | awk 'BEGIN{FS="/"}{print $NF}' | tr -d '\n' )
         copied_file="copy-$file_name"
@@ -105,16 +120,17 @@ function fromFile(){
         # grep $word1 $file_path | grep -n $word2
         grep $word1 $copied_file | grep $word2
         rm $copied_file
-        ;;
+        break
+				;;
       Quit)
         break
-        exit 0
-        ;;
+        break
+				;;
       *)
         echo "${tmagenta}Goodbye!${treset}"
         break
-        exit 1
-        ;;
+        break
+				;;
     esac
   done
 }
