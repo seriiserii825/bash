e49488840389b71f5a05dc6d9a15103f115c9c36
serii burduja <seriiburduja@gmail.com>
2025-06-07 11:51:17 +0300

    feat: added base packages

--- bash-scripts/pip.sh
+++ bash-scripts/pip.sh
@@ -1,18 +1,24 @@
 #! /bin/bash
 
+function installBasePackages(){
+  packages=("autopep8" "flake8")
+  for package in "${packages[@]}"; do
+    if ! python3 -m pip show $package > /dev/null 2>&1; then
+      installPackageByName $package
+    fi
+  done
+}
 function init(){
   current_dir=$(pwd)
   python3 -m venv venv
   source venv/bin/activate
   python3 -m pip install --upgrade pip
 }
-
 function activate(){
   source venv/bin/activate
   python3 -m ensurepip --upgrade
   python3 -m pip install --upgrade pip
 }
-
 function initIfNotExists(){
   if [ ! -d "venv" ]; then
     init
@@ -20,52 +26,71 @@ function initIfNotExists(){
     activate
   fi
 }
-
-function menu(){
-  echo "${tgreen}1. List${treset}"
-  echo "${tblue}2. Install Package${treset}"
-  echo "${tblue}3. Install all${treset}"
-  echo "${tmagenta}4. Uninstall${treset}"
-  echo "${tmagenta}5. Reinstall all${treset}"
-  echo "${tmagenta}6. Exit${treset}"
-
-  read -p "Enter the option: " option
-  case $option in
-    1)
-      initIfNotExists
-      bat requirements.txt
-      deactivate
-      menu
-      ;;
-    2)
-      read -p "Enter the package name: " package_name
-      initIfNotExists
-      python3 -m pip install $package_name
-      pip freeze > requirements.txt
-      deactivate
-      menu
-      ;;
-    3)
-      initIfNotExists
-      python3 -m pip install -r requirements.txt
-      deactivate
-      menu
-      ;;
-    4)
-      initIfNotExists
-      # package name from file requirements.txt with fzf
-      package_name=$(cat requirements.txt | fzf)
-      package_name=$(echo $package_name | cut -d'=' -f1)
-      python3 -m pip uninstall $package_name
-      pip freeze > requirements.txt
-      deactivate
-      menu
-      ;;
-    5)
+function listRequirements(){
+  initIfNotExists
+  if [ ! -f "requirements.txt" ]; then
+    echo "No requirements.txt found"
+  fi
+  bat requirements.txt
+  deactivate
+}
+function installPackageByName(){
+  if [ -n "$1" ]; then
+    package_name=$1
+  else
+    read -p "Enter the package name: " package_name
+  fi
+  initIfNotExists
+  python3 -m pip install $package_name
+  pip freeze > requirements.txt
+  deactivate
+}
+function installAllFromRequirements(){
+  initIfNotExists
+  python3 -m pip install -r requirements.txt
+  deactivate
+}
+function uninstallPackage(){
+  initIfNotExists
+  package_name=$(cat requirements.txt | fzf)
+  package_name=$(echo $package_name | cut -d'=' -f1)
+  python3 -m pip uninstall $package_name
+  pip freeze > requirements.txt
+  deactivate
+}
+function reinstallAll(){
       rm -rf venv
       initIfNotExists
       python3 -m pip install -r requirements.txt
       deactivate
+}
+function menu(){
+  echo "${tgreen}1. List${treset}"
+  echo "${tblue}2. Install Package by name${treset}"
+  echo "${tblue}3. Install all from requirements.txt${treset}"
+  echo "${tmagenta}4. Uninstall${treset}"
+  echo "${tmagenta}5. Reinstall all${treset}"
+  echo "${tmagenta}6. Exit${treset}"
+  read -p "Enter the option: " option
+  case $option in
+    1)
+      listRequirements
+      menu
+      ;;
+    2)
+      installPackageByName
+      menu
+      ;;
+    3)
+      installAllFromRequirements
+      menu
+      ;;
+    4)
+      uninstallPackage
+      menu
+      ;;
+    5)
+      reinstallAll
       menu
       ;;
     6)
@@ -77,5 +102,5 @@ function menu(){
       ;;
   esac
 }
-
+installBasePackages
 menu
