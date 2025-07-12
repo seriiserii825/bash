e62138d7d55c638db9bc969804b04965d32edf27
serii <seriiburduja@gmail.com>
2025-07-12 09:55:02 +0300

    feat: pythone code in inser_codeblock

--- bash-scripts/insert_codeblock.sh
+++ bash-scripts/insert_codeblock.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# get argument for language
+lang="$1"
+if [ -z "$lang" ]; then
+  lang="bash"  # Default to bash if no argument is provided
+fi
+
 # Get clipboard content
 content=$(xclip -selection clipboard -o)
 
 # Wrap in triple backticks with proper line breaks
 wrapped=$(cat <<EOF
-\`\`\`bash
+\`\`\`$lang
 ${content}
 \`\`\`
 EOF
