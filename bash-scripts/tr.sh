ace735a3233103762df219a6c4943e5b493a3e0d
serii burduja <seriiburduja@gmail.com>
2025-09-10 15:30:46 +0300

    feat: tr.sh update

--- bash-scripts/tr.sh
+++ bash-scripts/tr.sh
@@ -1,57 +1,32 @@
 #!/bin/bash
-# check if trans-shell is installed
-if ! [ -x "$(command -v trans)" ]
-then
-  echo "${tmagenta}trans-shell is not installed. Please install it first.${treset}"
-  sudo pacman -S gawk
-  sudo pacman -S trans-shell
+
+# Check if trans-shell is installed
+if ! command -v trans &> /dev/null; then
+  echo "trans-shell is not installed. Please install it first."
+  sudo pacman -S gawk trans-shell
 fi
-clipboard=$(xclip -o -selection clipboard)
-select lang in "en" "it" "ru" "ro" "de" "fr"
-do
-  case $lang in
-    "en")
-      trans -b :en "$clipboard" | tr -d '\n' | xsel -b -i 
-      copied_text=$(xsel -b -o)
-      echo "$copied_text"
-      break
-      ;;
-    "it")
-      trans -b :it "$clipboard"  | tr -d '\n' | xsel -b -i 
-      copied_text=$(xsel -b -o)
-      echo "$copied_text"
-      break
-      ;;
-    "ru")
-      trans -b :ru "$clipboard"  | tr -d '\n' | xsel -b -i 
-      copied_text=$(xsel -b -o)
-      echo "$copied_text"
-      break
-      ;;
-    "ro")
-      trans -b :ro "$clipboard"  | tr -d '\n' | xsel -b -i 
-      copied_text=$(xsel -b -o)
-      echo "$copied_text"
-      break
-      ;;
-    "de")
-      trans -b :de "$clipboard"  | tr -d '\n' | xsel -b -i 
-      copied_text=$(xsel -b -o)
-      echo "$copied_text"
-      break
-      ;;
-    "fr")
-      trans -b :fr "$clipboard"  | tr -d '\n' | xsel -b -i 
-      copied_text=$(xsel -b -o)
-      echo "$copied_text"
-      break
-      ;;
-    *)
-      echo "ERROR! Please select between 1..6";;
-  esac
+
+while true; do
+  clipboard=$(xclip -o -selection clipboard)
+
+  echo "Select target language:"
+  select lang in "en" "it" "ru" "ro" "de" "fr"; do
+    case $lang in
+      en|it|ru|ro|de|fr)
+        trans -b :$lang "$clipboard" | tr -d '\n' | xsel -b -i
+        copied_text=$(xsel -b -o)
+        echo "Translated ($lang): $copied_text"
+        break
+        ;;
+      *)
+        echo "ERROR! Please select between 1..6"
+        ;;
+    esac
+  done
+
+  read -p "Do you want to continue translating? (y/n): " choice
+  if [[ $choice != "y" ]]; then
+    echo "Exiting translator."
+    break
+  fi
 done
-echo "language selected: $lang"
-read -p "Write text to translate: " text
-trans -b :$lang "$text" | tr -d '\n' | xsel -b -i
-copied_text=$(xsel -b -o)
-echo "$copied_text"
