8537cf542352cdd157c4576ea8db0ab42708122e
serii burduja <seriiburduja@gmail.com>
2025-10-17 18:49:36 +0300

    feat: i3 update

--- bash-scripts/i3.sh
+++ bash-scripts/i3.sh
@@ -1,48 +1,49 @@
 #!/bin/bash
-#
+
 lock() {
     i3lock -i ~/Pictures/wallpapers/i3lock.png -t
 }
 
-select action in "lock" "logout" "suspend" "reboot" "shutdown" "cancel";
-do
+select action in "lock" "logout" "suspend" "reboot" "shutdown" "cancel"; do
     case $action in
         "lock")
             lock
             ;;
         "logout")
-            i3-msg exit
             read -p "Are you sure you want to logout? (y/n) " -n 1 -r
-            if [[ $REPLY =~ ^[Yy]$ ]]
-              i3-msg exit
+            echo
+            if [[ $REPLY =~ ^[Yy]$ ]]; then
+                i3-msg exit
             fi
             ;;
         "suspend")
-            lock && systemctl suspend
             read -p "Are you sure you want to suspend? (y/n) " -n 1 -r
-            if [[ $REPLY =~ ^[Yy]$ ]]
-              lock && systemctl suspend
+            echo
+            if [[ $REPLY =~ ^[Yy]$ ]]; then
+                lock && systemctl suspend
             fi
             ;;
         "reboot")
-            systemctl reboot
             read -p "Are you sure you want to reboot? (y/n) " -n 1 -r
-            if [[ $REPLY =~ ^[Yy]$ ]]
-              systemctl reboot
+            echo
+            if [[ $REPLY =~ ^[Yy]$ ]]; then
+                systemctl reboot
             fi
             ;;
         "shutdown")
-          read -p "Are you sure you want to shutdown? (y/n) " -n 1 -r
-          if [[ $REPLY =~ ^[Yy]$ ]]
-            systemctl poweroff
-          fi
+            read -p "Are you sure you want to shutdown? (y/n) " -n 1 -r
+            echo
+            if [[ $REPLY =~ ^[Yy]$ ]]; then
+                systemctl poweroff
+            fi
             ;;
         "cancel")
-          exit 0
-          ;;
+            echo "Cancelled."
+            exit 0
+            ;;
         *)
-            echo "Invalid option $REPLY"
-            exit 1
+            echo "Invalid option."
+            ;;
     esac
 done
 
