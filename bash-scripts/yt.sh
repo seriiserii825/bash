ead906995740a0d04e524067fc52e0d83be11e82
serii burduja <seriiburduja@gmail.com>
2026-02-09 18:43:52 +0200

    feat: yt.sh

--- bash-scripts/yt.sh
+++ bash-scripts/yt.sh
@@ -8,10 +8,12 @@ if [[ $youtube_url == *youtube* ]]; then
     video_id="${BASH_REMATCH[1]}"
     echo "${tgreen}Video ID: $video_id${treset}"
     # mpv $youtube_url &
-    smplayer $youtube_url &
+    mpv $youtube_url &
     # yt playurl $video_id
 elif [[ $youtube_url == *twitch* ]]; then
-    smplayer $youtube_url &
+    mpv $youtube_url &
+elif [[ $youtube_url == *vkvideo* ]]; then
+    mpv $youtube_url &
 else
     echo "${tmagenta}Error: Invalid YouTube URL.${treset}"
 fi
