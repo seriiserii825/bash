2c7358fceb50d98c2c00b4809a8643d1f6e2d06f
serii burduja <seriiburduja@gmail.com>
2026-02-11 09:51:05 +0200

    feat: acf select

--- bash-wp/acf-select.sh
+++ bash-wp/acf-select.sh
@@ -5,6 +5,7 @@ result_path=~/Downloads/slugs.txt
 touch "$file_path"
 touch $result_path
 
+# Read the clipboard content and save it to a file
 clipboard=$(xclip -o -selection clipboard)
 echo "$clipboard" >> "$file_path"
 bat "$file_path"
@@ -16,5 +17,6 @@ done < "$file_path"
 xclip -selection clipboard < "$result_path"
 bat "$result_path"
 
+# Cleanup
 rm "$file_path"
 rm "$result_path"
