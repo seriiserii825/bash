403bb2057d30589df4199ef95163f4d15d8e9387
seriiserii825 <seriiburduja@gmail.com>
2026-02-18 23:45:08 +0200

    feat: some

--- bash-scripts/mnt.sh
+++ bash-scripts/mnt.sh
@@ -4,20 +4,17 @@
 TARGET_UUID="bd51a002-3aea-4574-99c0-a98a67753b37"
 MOUNT_POINT="/mnt"
 
-# Use sudo with blkid in case root is required
 device=$(sudo blkid -o device -t UUID="$TARGET_UUID")
 
 if [ -n "$device" ]; then
-  echo "${tgreen}✅ Found target device: $device${treset}"
+  echo "✅ Found target device: $device"
   sudo mount "$device" "$MOUNT_POINT" && cd "$MOUNT_POINT"
   exec zsh
 else
-  echo "${tmagenta}⚠️ Target device not found. Choose manually.${treset}"
-
+  echo "⚠️ Target device not found. Choose manually."
   lsblk
-  devices=$(lsblk -dn -o NAME)
-
-  PS3="Please choose your device: "
+  devices=$(lsblk -ln -o NAME,TYPE | awk '$2=="part"{print $1}')
+  PS3="Please choose your partition: "
   COLUMNS=1
   select dev in $devices; do
     sudo mount /dev/$dev "$MOUNT_POINT" && cd "$MOUNT_POINT"
@@ -25,19 +22,3 @@ else
     break
   done
 fi
-# #! /bin/bash
-#
-# devices=$(cat /proc/partitions | awk '{print $NF}')
-# usb_uuid='bd51a002-3aea-4574-99c0-a98a67753b37'
-#
-# lsblk
-#
-# PS3="${tgreen}Please choose your device: ${treset}"
-# COLUMNS=1
-# select device in $devices
-# do
-#   sudo mount /dev/$device /mnt
-#   cd /mnt
-#   exec zsh
-#   exit 0
-# done
