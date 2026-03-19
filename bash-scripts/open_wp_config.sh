05a69b24e66d2b4c6d9b63750ab184144097691d
serii burduja <seriiburduja@gmail.com>
2026-03-19 18:44:06 +0200

    feat: open wp config

---
+++ bash-scripts/open_wp_config.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+TARGET="wp-config.php"
+DIR="$(pwd)"
+
+found=0
+
+while [[ "$DIR" != "/" ]]; do
+  FILE="$DIR/$TARGET"
+
+  if [[ -f "$FILE" ]]; then
+    found=1
+    SIZE=$(du -h "$FILE" | cut -f1)
+
+    echo "Found: $FILE"
+
+    read -rp "Copy file path to clipboard? (y/N): " answer
+    if [[ "$answer" =~ ^[Yy]$ ]]; then
+      : > "$FILE"
+      nvim "\"$FILE\""
+      # echo "$FILE"
+      # echo -n "\"$FILE\":" | xclip -selection clipboard
+      # echo "✔ File path copied to clipboard"
+    else
+      echo "✖ Skipped"
+    fi
+  fi
+
+  DIR="$(dirname "$DIR")"
+done
+
+if [[ $found -eq 0 ]]; then
+  echo "No debug.log found in parent directories"
+fi
