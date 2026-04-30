751c7d420a8ae0cbf0ea637cfd3781adce07f8ad
serii burduja <seriiburduja@gmail.com>
2026-04-30 15:33:15 +0300

    feat: yt_mp4 for vk

--- bash-scripts/yt_mp4.sh
+++ bash-scripts/yt_mp4.sh
@@ -8,6 +8,8 @@ if [[ $youtube_url == *youtube* ]]; then
   # else if zen
 elif [[ $youtube_url == *zen* ]]; then
   yt-dlp -o '%(title)s.%(ext)s' --format "bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 $youtube_url
+elif [[ $youtube_url == *vkvideo* || $youtube_url == *vk.com/video* ]]; then
+  yt-dlp -o '%(title)s.%(ext)s' --format "bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 $youtube_url
 else
   echo "${tmagenta}Error: Invalid YouTube URL.${treset}"
 fi
