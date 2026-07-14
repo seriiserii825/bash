286e46abfc937ef566833891cd494e39771f1555
serii burduja <seriiburduja@gmail.com>
2026-07-14 16:40:10 +0300

    feat: yt-open fix log

--- bash-scripts/yt-open.sh
+++ bash-scripts/yt-open.sh
@@ -9,12 +9,12 @@ if [[ $youtube_url == *youtube* ]]; then
     video_id="${BASH_REMATCH[1]}"
     echo "${tgreen}Video ID: $video_id${treset}"
     # mpv $youtube_url &
-    mpv $youtube_url &
+    mpv --msg-level=ffmpeg=no,cplayer=warn $youtube_url &
     # yt playurl $video_id
 elif [[ $youtube_url == *twitch* ]]; then
-    mpv $youtube_url &
+    mpv --msg-level=ffmpeg=no,cplayer=warn $youtube_url &
 elif [[ $youtube_url == *vkvideo* ]]; then
-    mpv $youtube_url &
+    mpv --msg-level=ffmpeg=no,cplayer=warn $youtube_url &
 else
     echo "${tmagenta}Error: Invalid YouTube URL.${treset}"
 fi
